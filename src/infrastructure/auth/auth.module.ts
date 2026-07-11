8e7f0288f600d8dfcea0a94d7077de1c3ee8230f
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-07-11 17:37:09 -0300

    update 17:31

--- src/infrastructure/auth/auth.module.ts
+++ src/infrastructure/auth/auth.module.ts
@@ -1,15 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtStrategy } from '../auth/jwt.strategy';
-import { APP_INTERCEPTOR } from '@nestjs/core/constants';
 import { TenantInterceptor } from '../http/tenant.interceptor';
+import { TenantContext } from '../database/tenant-context';
+import { PrismaService } from '../database/prisma.service';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   providers: [
     JwtStrategy,
+    TenantContext,
+    PrismaService,
     { provide: APP_INTERCEPTOR, useClass: TenantInterceptor },
   ],
-  exports: [PassportModule],
+  exports: [PassportModule, TenantContext],
 })
 export class AuthModule {}
