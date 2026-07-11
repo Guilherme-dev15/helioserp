315562b79c0dd044377fb1b818d29e4471d49669
guilherme-dev15 <guilherme.macedo1598@gmail.com>
2026-07-11 15:56:25 -0300

    chore: adiciona logs de debug no JwtAuthGuard

--- src/infrastructure/auth/jwt-auth.guard.ts
+++ src/infrastructure/auth/jwt-auth.guard.ts
@@ -1,9 +1,27 @@
-// src/infrastructure/auth/jwt-auth.guard.ts
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  Injectable,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-  // Aqui podemos no futuro adicionar a lógica de ler rotas @Public()
-  // para ignorar a autenticação no catálogo público.
+  handleRequest(err: any, user: any, info: any) {
+    // 👇 O Soro da Verdade: Imprime no console do Render o motivo real do bloqueio
+    if (info) {
+      console.error('🕵️ MOTIVO SECRETO DO BLOQUEIO:', info.message || info);
+    }
+    if (err) {
+      console.error('🕵️ ERRO INTERNO DO PASSPORT:', err);
+    }
+
+    if (err || !user) {
+      throw err || new UnauthorizedException();
+    }
+    return user;
+  }
 }
